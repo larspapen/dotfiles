c7c587b37bb5d7d6ba8a946ec63b45b60b1430eb
larspapen <lars@papen-kordt.de>
2023-02-22 20:41:01 +0100

    Update dotfiles

---
+++ dotfiles/fish/functions/config.fish
@@ -0,0 +1,51 @@
+set fish_greeting ""
+set -x EDITOR nvim
+
+
+switch (uname)
+    case Darwin
+            eval (/opt/homebrew/bin/brew shellenv)
+end
+
+# Aliases definitions
+alias l="ls -lahF"
+alias reload="source ~/.config/fish/config.fish"
+alias findBigFiles='du -hs $(ls)'
+
+alias ssh='TERM=xterm-256color command ssh'
+
+alias kclear='kubectx -u'
+alias k='kubectl'
+alias kctx='kubectx'
+alias kns='kubens'
+
+alias glg="git log --oneline --decorate --graph --all"
+alias gst="git status"
+alias gco="git checkout"
+alias gcp="git cherry-pick"
+alias gpf="git pull --all && git fetch --prune"
+alias gdb="git branch --merged | egrep -v '(^\*|master|dev|main)' | xargs
+git branch -d && git remote prune origin"
+
+# Application sourcing
+
+# z
+zoxide init fish | source
+# fnm
+fnm env --use-on-cd | source
+# kubectl
+kubectl completion fish | source
+# Starship rs
+starship init fish | source
+# Python pyenv
+pyenv init - | source
+pyenv virtualenv-init - | source
+
+# Container
+#alias docker=podman
+#export
+# DOCKER_HOST='unix:///Users/larspapen/.local/share/containers/podman/machine/podman-machine-default/podman.sock'
+
+# start stopped docker container with sh
+# docker run -it --entrypoint /bin/sh dmn
+
---
+++ setup_archlinux.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+# Isntall yay and packages
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd ..
+rm -rf yay-bin
+
+PACKAGES=(
+    bitwarden
+    discord
+    dolphin
+    fish
+    firefox
+    htop
+    kitty
+    kubectx
+    man
+    nerd-fonts-hasklig
+    openssh
+    reflector
+    telegram-desktop
+    ttf-apple-emoji
+    visual-studio-code-bin
+    wget
+    whois
+    zoxide
+    zip
+)
+
+yay
+yay -S $PACKAGES
+
+# git stuff
+REPO_PATH="/home/larspapen/dev/dotfiles"
+git clone https://github.com/larspapen/dotfiles.git $REPO_PATH
+
+git config --global core.excludesfile '~/.config/git/ignore'
+git config --global init.defaultBranch main
+git config --global pull.rebase true
+git config --global advice.statusHints false
+git config --global gpg.format ssh
+git config --global commit.gpgsign true
+git config --global user.signingkey 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK+aTK05grZ4V1JbtHqOD0j0KUWh4YAeoVL/Fw4PXqSh Lars@Papen-kordt.de'
+
+# Create dirs and link files
+mkdir "$HOME/dev"
+mkdir "$HOME/.ssh"
+mkdir "$HOME/.config"
+
+mkdir "$HOME/.config/fish"
+ln -sf "/Users/larspapen/dev/dotfiles/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+ln -sf "/Users/larspapen/dev/dotfiles/dotfiles/starship/starship.toml" "$HOME/.config/starship.toml"
+ln -sf "/Users/larspapen/dev/dotfiles/dotfiles/fish/config.fish" "$HOME/.config/fish/config.fish"
+ln -sf "/Users/larspapen/dev/dotfiles/dotfiles/fish/functions" "$HOME/.config/fish/functions"
+
+# Link folders
+ln -sf "/Users/larspapen/dev/dotfiles/dotfiles/git" "$HOME/.config/git"
